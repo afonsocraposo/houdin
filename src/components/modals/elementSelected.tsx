e5b7fa8abba7892311e46dbccff5a0563522edb3
Afonso Raposo <afonsocraposo@gmail.com>
2025-08-21 11:12:45 +0100

    fix: key press trigger and modal zindex (#13)
    * fix: key press trigger multiple times
    
    * fix: show modals on top
    
    * chore: bump version to 3.1.1
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/components/modals/elementSelected.tsx
+++ src/components/modals/elementSelected.tsx
@@ -23,7 +23,7 @@ export default function ElementSelectedModal({
       onClose={close}
       title="Element Inspector"
       trapFocus={false}
-      zIndex={1000} // Ensure modal is on top
+      zIndex={1000000} // Ensure modal is on top
     >
       <Stack justify="stretch">
         <InfoRow label="Selector" value={selector} />
