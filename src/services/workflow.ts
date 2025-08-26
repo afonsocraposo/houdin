3530cc6062e8e2cc116327c5bbdc0fa0546783da
Afonso Raposo <afonsocraposo@gmail.com>
2025-08-26 09:45:47 +0100

    chore: remove console.log

--- src/services/workflow.ts
+++ src/services/workflow.ts
@@ -169,7 +169,6 @@ export class WorkflowExecutor {
         //@ts-ignore
         const value = actionConfig[key];
         if (typeof value === "string") {
-          console.log("Interpolating", key, value);
           //@ts-ignore
           actionConfig[key] = this.context.interpolateVariables(value);
         }
