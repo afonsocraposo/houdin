900d46a70c77bf7b3ebd4b724b9c89b2dbc34172
Afonso Raposo <afonsocraposo@gmail.com>
2025-10-20 22:45:03 +0100

    test: e2e tests for buttons triggers

--- tests/triggers.spec.ts
+++ tests/triggers.spec.ts
@@ -1,3 +1,5 @@
+import { DEMO_BUTTON_WORKFLOW } from "./demoWorkflows/button";
+import { DEMO_FAB_WORKFLOW } from "./demoWorkflows/fab";
 import { DEMO_HTTP_REQUEST_TRIGGER_WORKFLOW } from "./demoWorkflows/httpRequestTrigger";
 import { test, expect } from "./test.base";
 import { importWorkflow } from "./utils";
@@ -16,5 +18,23 @@ test.describe("Actions execution", () => {
       fetch("https://api.ipify.org/");
     });
     await expect(page.locator('text="Request detected"')).toBeVisible();
+  });
+
+  test("can trigger workflow on fab click", async ({ page, baseUrl }) => {
+    await importWorkflow(baseUrl, page, DEMO_FAB_WORKFLOW);
+
+    await page.goto("https://example.com");
+
+    await page.click('text="FB"');
+    await expect(page.locator('text="Workflow triggered"')).toBeVisible();
+  });
+
+  test("can trigger workflow on button click", async ({ page, baseUrl }) => {
+    await importWorkflow(baseUrl, page, DEMO_BUTTON_WORKFLOW);
+
+    await page.goto("https://example.com");
+
+    await page.click('text="FB"');
+    await expect(page.locator('text="Workflow triggered"')).toBeVisible();
   });
 });
