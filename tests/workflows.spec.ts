c13aef5088aed2929323096978027101a6589e53
Afonso Raposo <afonsocraposo@gmail.com>
2025-10-22 22:13:10 +0100

    test: add e2e test for liquid syntax parsing

--- tests/workflows.spec.ts
+++ tests/workflows.spec.ts
@@ -6,6 +6,7 @@ import {
 } from "../src/types/workflow";
 import { Destinations, importWorkflow, UrlBuilder } from "./utils";
 import { DEMO_META_VARIABLES_WORKFLOW } from "./demoWorkflows/metaVariables";
+import { DEMO_LIQUID_WORKFLOW } from "./demoWorkflows/liquid";
 
 test.describe("Workflows creation, design and execution", () => {
   // always go to config page before each test
@@ -253,5 +254,17 @@ test.describe("Workflows creation, design and execution", () => {
 
     await expect(page.locator('text="meta.url"')).toBeVisible();
     await expect(page.locator('text="https://example.com/"')).toBeVisible();
+  });
+
+  test("can use liquid syntax with variables", async ({ page, baseUrl }) => {
+    await importWorkflow(baseUrl, page, DEMO_LIQUID_WORKFLOW);
+
+    // Go to example.com
+    await page.goto("https://example.com");
+
+    await expect(page.locator('text="meta url with liquid"')).toBeVisible();
+    await expect(
+      page.locator('text="https://example.com/#test"'),
+    ).toBeVisible();
   });
 });
