b218869337dde5c3144abfd1152636e44834d0c3
Afonso Raposo <afonsocraposo@gmail.com>
2025-10-23 20:58:35 +0100

    feat: add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import { Box, Stack, Text, Anchor } from "@mantine/core";
+
+export default function Footer() {
+  return (
+    <Box py="lg" mt="xl">
+      <Stack>
+        <Text ta="center" c="dimmed" size="sm">
+          Made with 🧡 by{" "}
+          <Anchor href="https://afonsoraposo.com" target="_blank">
+            Afonso Raposo
+          </Anchor>
+        </Text>
+        <Text ta="center" c="dimmed" size="sm">
+          <Anchor
+            href="https://github.com/afonsocraposo/houdin"
+            target="_blank"
+          >
+            Github
+          </Anchor>
+          {" • "}
+          <Anchor
+            href="https://github.com/afonsocraposo/houdin/blob/main/LICENSE.md"
+            target="_blank"
+          >
+            PolyForm Noncommercial License
+          </Anchor>
+        </Text>
+      </Stack>
+    </Box>
+  );
+}
