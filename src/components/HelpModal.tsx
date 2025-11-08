77f09fa3e3a28a2a69026042990749aa7626612a
Afonso Raposo <afonso.raposo@paddle.com>
2025-11-08 13:20:02 +0000

    feat: add email text

--- src/components/HelpModal.tsx
+++ src/components/HelpModal.tsx
@@ -1,17 +1,23 @@
-import { Modal, ModalProps } from "@mantine/core";
+import { Anchor, Modal, ModalProps, Stack, Text, Title } from "@mantine/core";
 
-interface HelpModalProps extends ModalProps {}
-export default function HelpModal(props: HelpModalProps) {
+export default function HelpModal(props: ModalProps) {
   return (
-    <Modal {...props} size="auto" title="Contact us">
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSfH9Wxu0dxcc6X-B9lEmA4Aop5AIfH4Mr1q7YjER8KmQkZR-g/viewform?embedded=true"
-        width="640"
-        height="880"
-        style={{ maxHeight: "75vh" }}
-      >
-        Loading...
-      </iframe>
+    <Modal {...props} size="auto" title={<Title>Contact us</Title>}>
+      <Stack>
+        <Text>
+          You can also send an email to{" "}
+          <Anchor href="mailto:help@houdin.dev">help@houdin.dev</Anchor>.
+        </Text>
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfH9Wxu0dxcc6X-B9lEmA4Aop5AIfH4Mr1q7YjER8KmQkZR-g/viewform?embedded=true"
+          width="640"
+          height="880"
+          style={{ maxHeight: "75vh" }}
+          title="Contact form"
+        >
+          Loading...
+        </iframe>
+      </Stack>
     </Modal>
   );
 }
