0b48a7fd959e30e5b2e95537589dd9ee71be3419
Afonso Raposo <afonsocraposo@gmail.com>
2025-12-19 19:59:42 +0000

    fix: detection of background

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -3,7 +3,10 @@ export async function isBackgroundContext(): Promise<boolean> {
   try {
     const background = await browser.runtime.getBackgroundPage();
     return window === background;
-  } catch {
+  } catch (e) {
+    if (typeof window !== "undefined") {
+      return false;
+    }
     return true;
   }
 }
